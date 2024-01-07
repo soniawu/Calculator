4d672a20cb1e6fe81c7a6f2dae7b0ddd9480b6eb
soniawu <sonia.wu.0906@gmail.com>
2024-01-06 19:55:46 -0600

    Add all operation handling Version 3 - Add handling positive/negative for numbers - Improve divided by 0 error handling. Give message instead   just returning 0. - Clear out display message when a new operarion starts.

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Version 2
 - Set up operate function to determine what operator input and cal the right function.
 - Set up add, subtrac, multiply and divide functions to do it's operation.
 - In this version, only sunny day scenario is in place.
+
+Version 3
+- Add handling positive/negative for numbers
+- Improve divided by 0 error handling. Give message instead just return 0.
+- Clear out display message when a new operarion starts.
--- js/calculator.js
+++ js/calculator.js
@@ -5,23 +5,24 @@ let opd_2; // operand 1
 let opr;   // operator
 let numStr = ""; // the number input as a string 
 let operator = 0; // operator for an action 0=none, 1=+, 2=-, 3=X, 4=/
+let newOper = true; // indicator of new operation starts
 let onOpd_2 = false; // indicator of operand 2 started
+let decmOn = false;  // indicator if decimal point is already entered
+let isPos = true;    // indicator for the current number is positive
+
 const displayContents = document.querySelector("#display>p");
 
-//console.log(`display is ${displayContents.innerHTML}`);
-//console.log(`the length is ${numElms.length}`);
-
-/* !!!!
-    Integer only sunny day condition. no +/- or .
-    to be considered.
-    !!!!
-    should add a check for the first number for operand 1 entered
-    to clear out display
-Event listeners for all number buttons
+/* 
+    Event listeners for all number buttons
 */ 
 const numElms = document.querySelectorAll(".num");
 for (let i = 0; i < numElms.length; i++) {
     numElms[i].addEventListener("click", () => {
+        if (newOper) {
+            console.log("This is new operation");
+            displayContents.innerHTML = "";
+            newOper = false;
+        }
         /* 
             if operator is already entered and 2nd operand indicator is false,
             that means this is the first number for the 2nd operand. clear out 
@@ -34,10 +35,10 @@ for (let i = 0; i < numElms.length; i++) {
             displayContents.innerHTML = "";
         }
 
-        let n = numElms[i].innerHTML;
-        displayContents.innerHTML += n;
-        numStr += n;
-    });
+    let n = numElms[i].innerHTML;
+    displayContents.innerHTML += n;
+    numStr += n;
+    })
 }
 
 /*
@@ -45,7 +46,8 @@ for (let i = 0; i < numElms.length; i++) {
     Delete the last number from display and the numStr
 
     !!! should think about what to do if operator is entered
-    and backOne key is clicked.
+    and backOne key is clicked. Currently, backing the last 
+    digit of operand 1. 
 */
 
 const backOne = document.querySelector("#backOne");
@@ -64,9 +66,8 @@ cancelAll.addEventListener("click", () => {
 });
 
 /*
-    Monitor all operators
+    Event listeners for operators
 */
-
 const numOprt = document.querySelectorAll(".operator");
 for (let i = 0; i < numOprt.length; i++) {
     numOprt[i].addEventListener("click", () => {
@@ -80,18 +81,23 @@ for (let i = 0; i < numOprt.length; i++) {
         */
         if ((operator != 0) && (onOpd_2)) {
             opd_2 = setOperand();
-            let n = operate(opd_1, opd_2, operator);
-            numStr = n.toString();
-            console.log(`numStr is ${numStr}`);
-            displayContents.innerHTML = numStr;
-            opd_1 = n;
-            opd_2 = 0;
-            onOpd_2 = false;
-            operator = 0;
+            let result = operate(opd_1, opd_2, operator);
+            if (typeof result === "number") {
+                numStr = result.toString();
+                console.log(`numStr is ${numStr}`);
+                displayContents.innerHTML = numStr;
+                opd_1 = result;
+                opd_2 = 0;
+                onOpd_2 = false;
+                operator = 0;
+            } else {
+                resetGlob;
+                displayContents.innerHTML = result;
+                console.log("I am here");
+            }
         }
 
         let theID = numOprt[i].getAttribute("id"); 
-        //console.log(`id is ${theID}`);
         switch (theID) {
             case "plus": operator = 1;
                 break;
@@ -103,17 +109,23 @@ for (let i = 0; i < numOprt.length; i++) {
                 break;
             default: operator = 0
         }
-        //console.log(`operator is ${operator}`);
     });
 }
 
+/*
+    Event listener for equal sign
+*/
 const equal = document.querySelector("#equal");
 equal.addEventListener("click", () =>{
     if (onOpd_2) {
         opd_2 = setOperand();
         let result = operate(opd_1, opd_2, operator);
         resetGlob();
-        displayContents.innerHTML = result.toString();
+        if (typeof result === "number") {
+            displayContents.innerHTML = result.toString();
+        } else {
+            displayContents.innerHTML = result;
+        }
     } else {
         // operand 2 is not yet entered, no operation.
         // return what had been entered
@@ -121,17 +133,59 @@ equal.addEventListener("click", () =>{
     }
 });
 
-/* 
+/*
+    Event listener for decimal point
+*/
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", () => {
+    /* decimal point is not allowed to entered at the 
+        beginning of the numbeer or it is already 
+        entered for this Number
+    */
+    if ((numStr !== "") && ( !decmOn)) {
+        displayContents.innerHTML += ".";
+        numStr += ".";
+        decmOn = true;
+    }
+});
+
+/*
+    Event listener for positive/negative sign
+*/
+const sign = document.querySelector(".posOrNeg");
+sign.addEventListener("click", () => {
+    /* 
+        Check the current indicator and change to opposite
+    */
+    if ((numStr !== "" ) || ((operator != 0) && (!onOpd_2)))
+    { 
+        if (isPos) {
+            numStr = "-" + numStr;
+        } else {
+            numStr = numStr.substring(1, numStr.length);
+        }
+        displayContents.innerHTML = numStr;
+        isPos = !isPos;
+    }
+});
+
+/**********************************************************
     It's sunny day condition now. should take care of veridation
     later in this function or separated function.
-
-    and for decimal numbers too
-*/
+**********************************************************/
 function setOperand() {
-    let n = Number(numStr);
-    //console.log(`operand is ${n}`);
-    return n;
+    let l = numStr.length - 1;
+    if (numStr[l] === ".") {
+        numStr += "00";
+    }
+    decmOn = false;
+    console.log(`operand is ${Number(numStr)}`);
+    return Number(numStr);
 }
+
+/**********************************************************
+    This function resets the global variables for a new operation
+**********************************************************/
 
 // reset global variables to the beginning
 function resetGlob() {
@@ -141,9 +195,15 @@ function resetGlob() {
    numStr = "";
    operator = 0; 
    onOpd_2 = false; 
+   decmOn = false;
+   newOper = true;
    displayContents.innerHTML = "";
 }
 
+/**********************************************************
+    This function handles the operation base on the current 
+    operator. 
+**********************************************************/
 function operate(a, b, o) {
     switch (o) {
         case 1: 
@@ -161,21 +221,34 @@ function operate(a, b, o) {
     }
 }
 
+/**********************************************************
+    This function performs addition operation
+**********************************************************/
 function add(a, b) {
     return (a + b);
 }
 
+/**********************************************************
+    This function performs subtraction operation
+**********************************************************/
 function subtract(a, b) {
     return (a - b);
 }
 
+/**********************************************************
+    This function performs multification operation
+**********************************************************/
 function multiply(a, b) {
     return (a * b);
 }
 
+/**********************************************************
+    This function performs division operation
+**********************************************************/
 function divide(a, b) {
+    // !!! add addiional care for divided by 0 case to display N/A
     if (b === 0) {
-        return 0;
+        return "N/A, AC to restart";
     } else {
         return (a / b);
     }
